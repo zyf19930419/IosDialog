85a75e85fb4f28ca31eabe05b9ca2d0f54d8b97f
zhangyunfei <32457127@qq.com>
2017-08-15 11:59:48 +0800

    仿iOS加载框

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -13,7 +13,6 @@ import com.example.app.view.LoadingDialog;
 import com.example.app.view.PhotoDialog;
 
 public class MainActivity extends AppCompatActivity {
-//    上下文对象
     private MainActivity selfActivity = MainActivity.this;
 
     private HintDialog hintDialog = new HintDialog(); // 提示框
